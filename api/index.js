097b3447009315e3e6b2901d06b32f22c0a3388d
Hemanth Mandapati <hemanth.mandapati01@gmail.com>
2023-06-24 00:54:10 -0700

    Implementing Chat functionality and other REST endpoints

--- api/index.js
+++ api/index.js
@@ -3,7 +3,11 @@ const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser')
 const User = require('./models/User')
 const jwt = require('jsonwebtoken');
-const cors = require('cors')
+const cors = require('cors');
+const bcrypt = require('bcryptjs');
+const Message = require('./models/Message');
+const ws = require('ws');
+const fs = require('fs');
 require('dotenv').config();
 
 const connectToMongo = async () => {
@@ -22,6 +26,7 @@ module.exports = connectToMongo;
 //     if (err) throw err;
 // });
 const jwtSecret = process.env.JWT_SECRET;
+const bcryptSalt = bcrypt.genSaltSync(10);
 
 const app = express();
 app.use(express.json());
@@ -29,9 +34,40 @@ app.use(cookieParser());
 app.use(cors({
     credentials: true,
     origin: process.env.CLIENT_URL,
-}))
+}));
+
+async function getUserDataFromRequest(req) {
+  return new Promise((resolve, reject) => {
+    const token = req.cookies?.token;
+    if (token) {
+      jwt.verify(token, jwtSecret, {}, (err, userData) => {
+        if (err) throw err;
+        resolve(userData);
+      });
+    } else {
+      reject('no token');
+    }
+  });
+
+}
 app.get('/test', (req, res) => {
     res.json('test ok');
+});
+
+app.get('/messages/:userId', async (req,res) => {
+  const {userId} = req.params;
+  const userData = await getUserDataFromRequest(req);
+  const ourUserId = userData.userId;
+  const messages = await Message.find({
+    sender:{$in:[userId,ourUserId]},
+    recipient:{$in:[userId,ourUserId]},
+  }).sort({createdAt: 1});
+  res.json(messages);
+});
+
+app.get('/people', async (req,res) => {
+  const users = await User.find({}, {'_id':1,username:1});
+  res.json(users);
 });
 
 app.get('/profile', (req, res) => {
@@ -45,6 +81,26 @@ app.get('/profile', (req, res) => {
         res.status(401).json('no token');
     }
 });
+
+app.post('/login', async (req,res) => {
+  const {username, password} = req.body;
+  const foundUser = await User.findOne({username});
+  if (foundUser) {
+    const passOk = bcrypt.compareSync(password, foundUser.password);
+    if (passOk) {
+      jwt.sign({userId:foundUser._id,username}, jwtSecret, {}, (err, token) => {
+        res.cookie('token', token, {sameSite:'none', secure:true}).json({
+          id: foundUser._id,
+        });
+      });
+    }
+  }
+});
+
+app.post('/logout', (req,res) => {
+  res.cookie('token', '', {sameSite:'none', secure:true}).json('ok');
+});
+
 app.post('/register', async(req, res) => {
     const {username, password} = req.body;
     try {
